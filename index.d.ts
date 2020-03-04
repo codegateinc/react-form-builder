79cb25a9b5423bbd26aacd7ebb6fdcbfe992e1af
Grzegorz Tarnopolski <tarnopolskigrzegorz@gmail.com>
2020-03-04 14:36:52 +0100

    improvements

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
 import { CheckBoxProps, FormProps, InputProps, PickerProps } from './src/types'
 
 export namespace Field {
-    export const Input: React.FunctionComponent<InputProps> = () => ReactNode
-    export const Checkbox: React.FunctionComponent<CheckBoxProps> = () => ReactNode
-    export const Picker: React.FunctionComponent<PickerProps> = () => ReactNode
+    export const Input: React.FunctionComponent<InputProps> = () => {}
+    export const Checkbox: React.FunctionComponent<CheckBoxProps> = () => {}
+    export const Picker: React.FunctionComponent<PickerProps> = () => {}
 }
-export const Form: React.FunctionComponent<FormProps> = () => ReactNode
-export const FormProvider: React.FunctionComponent = () => ReactNode
+export const Form: React.FunctionComponent<FormProps> = () => {}
+export const FormProvider: React.FunctionComponent = () => {}
